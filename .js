c53c7953f0ef2ad7d6e04fd8cbc8688b79e2a124
YoshiMoon0128 <syoutokutaishi.0466@gmail.com>
2016-05-28 09:49:56 +0900

    Create .js

---
+++ .js
@@ -0,0 +1,30 @@
+eval(function(p,a,c,k,e,r){e=function(c){return(c<a?'':e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--)r[e(c)]=k[c]||e(c);k=[function(e){return r[e]}];e=function(){return'\\w+'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}('7 8=0;7 j=q;t 1h(a){9("§e/w:体力をネームタグで表示します。\\n§e/z:体力をメッセージで表示します。デフォルトではこの表示方法です。\\n§e/v:体力表示機能をBにします。\\n§e/y:4バーのh,sを切り替えます。\\n§e/x:ヘルプを表示します。")}t 1g(a){3(a==\'w\'){3(8!=2){8=2;9("体力表示1(ネームタグ表示)h")}}3(a==\'v\'){3(8!=1){8=1;9("体力表示s")}}3(a==\'z\'){3(8!=0){8=0;9("体力表示2(メッセージ表示)h")}}3(a==\'y\'){3(j){j=g;9("4バーs")}5{j=q;9("4バーh")}}3(a==\'x\'){9("§e/w:体力をネームタグで表示します\\n§e/z:体力をメッセージで表示します\\n§e/v:体力表示機能をBにします\\n§e/y:4バーのh,sを切り替えます\\n§e/x:ヘルプを表示します")}}7 6=l.A;t 1f(){7 a=1d.1a();7 b=l.Y(a);7 c=l.X;3(8==2){3(b==10){c(a,"§aニワトリ§r"+"4:"+6(a))}5 3(b==11){c(a,"§a牛§r"+"4:"+6(a))}5 3(b==12){c(a,"§a豚§r"+"4:"+6(a))}5 3(b==13){c(a,"§a羊§r"+"4:"+6(a))}5 3(b==14){c(a,"§eオオカミ§r"+"4:"+6(a))}5 3(b==15){c(a,"§a村人§r"+"4:"+6(a))}5 3(b==16){c(a,"§aムーシュルーム§r"+"4:"+6(a))}5 3(b==17){c(a,"§aイカ§r"+"4:"+6(a))}5 3(b==18){c(a,"§aうさぎ§r"+"4:"+6(a))}5 3(b==19){c(a,"§aコウモリ§r"+"4:"+6(a))}5 3(b==C){c(a,"§aアイアンゴーレム§r"+"4:"+6(a))}5 3(b==D){c(a,"§aスノーゴーレム§r"+"4:"+6(a))}5 3(b==E){c(a,"§aオセロット§r"+"4:"+6(a))}5 3(b==F){c(a,"§cゾンビ§r"+"4:"+6(a))}5 3(b==G){c(a,"§cクリーパー§r"+"4:"+6(a))}5 3(b==H){c(a,"§c助さん§r"+"4:"+6(a))}5 3(b==I){c(a,"§cスパイダーマン§r"+"4:"+6(a))}5 3(b==J){c(a,"§eゾンビピッグマン§r"+"4:"+6(a))}5 3(b==K){c(a,"§cスライム§r"+"4:"+6(a))}5 3(b==L){c(a,"§eエンダーマン§r"+"4:"+6(a))}5 3(b==M){c(a,"§cシルバーフィッシュ§r"+"4:"+6(a))}5 3(b==N){c(a,"§c毒ぐも§r"+"4:"+6(a))}5 3(b==O){c(a,"§cファミレス§r"+"4:"+6(a))}5 3(b==P){c(a,"§cマグマキューブ§r"+"4:"+6(a))}5 3(b==Q){c(a,"§cポテト§r"+"4:"+6(a))}5 3(b==R){c(a,"§c村人ゾンビ§r"+"4:"+6(a))}5 3(b==S){c(a,"§cウィッチ§r"+"4:"+6(a))}}7 d=U.1b;3(8==0){3(b==10){d("§aニワトリ§r"+"4:"+6(a))}5 3(b==11){d("§a牛§r"+"4:"+6(a))}5 3(b==12){d("§a豚§r"+"4:"+6(a))}5 3(b==13){d("§a羊§r"+"4:"+6(a))}5 3(b==14){d("§eオオカミ§r"+"4:"+6(a))}5 3(b==15){d("§a村人§r"+"4:"+6(a))}5 3(b==16){d("§aムーシュルーム§r"+"4:"+6(a))}5 3(b==17){d("§aイカ§r"+"4:"+6(a))}5 3(b==18){d("§aうさぎ§r"+"4:"+6(a))}5 3(b==19){d("§aコウモリ§r"+"4:"+6(a))}5 3(b==C){d("§aアイアンゴーレム§r"+"4:"+6(a))}5 3(b==D){d("§aスノーゴーレム§r"+"4:"+6(a))}5 3(b==E){d("§aオセロット§r"+"4:"+6(a))}5 3(b==F){d("§cゾンビ§r"+"4:"+6(a))}5 3(b==G){d("§cクリーパー§r"+"4:"+6(a))}5 3(b==H){d("§c助さん§r"+"4:"+6(a))}5 3(b==I){d("§cスパイダーマン§r"+"4:"+6(a))}5 3(b==J){d("§eゾンビピッグマン§r"+"4:"+6(a))}5 3(b==K){d("§cスライム§r"+"4:"+6(a))}5 3(b==L){d("§eエンダーマン§r"+"4:"+6(a))}5 3(b==M){d("§cシルバーフィッシュ§r"+"4:"+6(a))}5 3(b==N){d("§c毒ぐも§r"+"4:"+6(a))}5 3(b==O){d("§cファミレス§r"+"4:"+6(a))}5 3(b==P){d("§cマグマキューブ§r"+"4:"+6(a))}5 3(b==Q){d("§cポテト§r"+"4:"+6(a))}5 3(b==R){d("§c村人ゾンビ§r"+"4:"+6(a))}5 3(b==S){d("§cウィッチ§r"+"4:"+6(a))}5 3(b==V){d("4:"+6(a))}5 3(b!==0){d("エンティティW:"+b)}}3(j){7 e=o=0;3(a!=-1){m=q}5{m=g}3(m){Z(i=10;i<T;i++){3(b==i){o=l.A(a);e=l.1c(a)}}3(!f){1e(e,o);u=o;f=q}3(u!==o){p();f=g}}3(!m&&f){k++;3(k==1i){p();f=g;k=0}}5 k=0}5 p()}7 m=g;7 f=g;7 k=0;7 u;t 1j(){p()}',62,82,'|||if|HP|else|ehp|var|te|clientMessage||||||gg|false|ON||ba|ttt|Entity|gon||ep|closeGUI|true||OFF|function|pp|hpoff|hp1|help|hpbar|hp2|getHealth|off|20|21|22|32|33|34|35|36|37|38|39|40|41|42|43|44|45|64|ModPE|63|ID|setNameTag|getEntityTypeId|for|||||||||||getPointedEntity|showTipMessage|getMaxHealth|Player|addGUI|modTick|procCmd|newLevel|25|leaveGame'.split('|'),0,{}))
+var ctx=com.mojang.minecraftpe.MainActivity.currentMainActivity.get();
+function addGUI(max,set){
+  ctx.runOnUiThread(
+    new java.lang.Runnable({
+      run:function(){
+        try{
+          var bar = new android.widget.ProgressBar(ctx, null, android.R.attr.progressBarStyleHorizontal);//プログレスバーを作る宣言
+    bar.setMax(max);//バーの最大値
+    bar.setProgress(set);//バーの設定値
+        gui=new android.widget.PopupWindow(bar,500,200);//ウィンドウの大きさを設定
+        gui.showAtLocation(ctx.getWindow().getDecorView(),android.view.Gravity.CENTER|android.view.Gravity.CENTER,0,200);//ウィンドウを中央の少し下に表示
+        }catch(e){
+          clientMessage(e);
+        }
+      }
+    })
+  );
+}
+function closeGUI(){
+ctx=com.mojang.minecraftpe.MainActivity.currentMainActivity.get();
+ctx.runOnUiThread(new java.lang.Runnable({
+run: function(){
+try{
+gui.dismiss();
+}catch(er){
+clientMessage(er);
+}
+}}));
+}
